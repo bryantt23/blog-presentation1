d5ee84e774150141af6feab3c5b88e27de7452b3
bryantt23 <bryantt23@gmail.com>
2015-03-14 22:18:17 -0700

    5d

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,7 @@
 class ArticlesController < ApplicationController
 
   def new
+    @article = Article.new
   end
 
   def index
@@ -12,17 +13,40 @@ class ArticlesController < ApplicationController
     @article=Article.find(params[:id])
   end
 
-  def create
-    @article = Article.new(article_params)
-
-    @article.save
-    redirect_to @article
+  def edit
+    @article=Article.find(params[:id])
   end
 
-  private
-    def article_params
-      params.require(:article).permit(:title, :text)
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
     end
+  end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
+  end
+
+
+  private
+  def article_params
+    params.require(:article).permit(:title, :text)
+  end
 
 
 end
